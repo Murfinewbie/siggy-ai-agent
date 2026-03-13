89c4d921c6125daaeb7137c976debe607b23b4ea
Unaniu <unaniu9@gmail.com>
2026-03-13 11:30:57 +0700

    fix types

--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
         })
       });
 
-      const data = await res.json();
+      const data: any = await res.json();
 
       setIsTyping(false);
 
@@ -267,7 +267,7 @@ export default function Home() {
         })
       });
 
-      const data = await res.json();
+      const data: any = await res.json();
 
       setIsTyping(false);
 
